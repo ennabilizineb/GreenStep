8936e86fea868587a9e48effbff972ebee52c823
ennabilizineb <ennabilizineb7@gmail.com>
2026-06-24 11:42:33 +0800

    Login View Styling

--- Frontend/src/views/LoginView.vue
+++ Frontend/src/views/LoginView.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
+import '../assets/main.css'
 
 const router = useRouter()
 const authoStore = useAuthStore()
